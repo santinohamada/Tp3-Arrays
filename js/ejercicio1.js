2774364c688b02953990a30f36f9d2e58e84f6ee
SantinoHamada <hamadasantino@hotmail.com>
2024-07-10 14:55:36 -0300

    se realizó el ejercicio 3 y se formateo los demas archivos

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -3,13 +3,24 @@
 Output:
 */
 
-const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", 
-"Junio", "Julio", "Agosto", "Septiembre", 
-"Octubre", "Noviembre", "Diciembre"]
+const meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
 
-document.write('<h1> Lista de meses</h1> ');
-document.write('<ul>');
-for(let i=0;i<meses.length;i++){
-    document.write(`<li> ${meses[i]} </li>`);
+document.write("<h1> Lista de meses</h1> ");
+document.write("<ul>");
+for (let i = 0; i < meses.length; i++) {
+  document.write(`<li> ${meses[i]} </li>`);
 }
-document.write('</ul>');
+document.write("</ul>");
